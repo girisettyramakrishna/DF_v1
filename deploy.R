acfd28c11818cc579cffbdd52f27a436f4918e40
Ramakrishna G <eravi1702@gmail.com>
2025-04-22 20:45:06 +0530

    deploy

---
+++ deploy.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(name='psmlabs',
+                          token='8FAB74BDBB46C9CA05D75B7102711773',
+                          secret='9a6AwJZn3QacJOlVCdzWv6Ptj34mOsuIRE3OuNDI')
+
+rsconnect::deployApp(appDir = '.', appName = 'DF_v123')
+
